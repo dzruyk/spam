ca363ebea3be6668e5592141c8590b12044b4bf9
Boris <sexyborka@mail.ru>
2013-06-05 02:02:19 +0400

    some additions

--- small_projs/my_decode.py
+++ small_projs/my_decode.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 
-from optparse import OptionParser
 import re
 import sys
 import urllib2
-
+from optparse import OptionParser
+from HTMLParser import HTMLParser
 
 baseurl="http://www.lingvo-online.ru/ru/Translate/en-ru/"
 
@@ -16,12 +16,41 @@ def usage():
 def debug(msg):
     print msg
 
+class MyTestParser(HTMLParser):
+    def __init__(self):
+        HTMLParser.__init__(self)
+        self.recording = 0
+        self.data = []
+    def handle_data(self, data):
+        pass
+    def handle_starttag(self, tag, attrs):
+        if tag != 'div':
+            return
+       
+        if u'js-article-html g-card' in attrs[0]:
+            debug("div finded, attrs %s" %attrs)
+    def handle_endtag(self, tag):
+        pass
+
 def get_description(url):
     
     try:
-        page = urllib2.urlopen(url).read()
+        c = urllib2.urlopen(url)
+        encoding = c.headers.getparam('charset')
+        page = c.read().decode(encoding)
     except KeyboardInterrupt:
+        print "[*] Interpreted"
+        exit(1)
     except BaseException:
+        print "[-] Could not connect to %s" % baseurl
+        exit(1)
+
+    p = MyTestParser()
+    p.feed(page)
+    p.close()
+
+    desc = "Stub now"
+    return desc
 
 def create_url(translateme):
     translateme = re.sub("\s+", "%20", translateme)
@@ -29,6 +58,7 @@ def create_url(translateme):
     return url
 
 def main(argc, argv):
+    debug("len of argc = %d" % argc)
     if (argc < 2):
         usage()
         exit(1)
