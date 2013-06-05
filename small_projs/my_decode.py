3bc66a716b6c40263e943cbff04aabaf12259fa3
Ruyk <root@ruykpc.rk.local>
2013-06-05 14:17:50 +0400

    some temp fixes

--- small_projs/my_decode.py
+++ small_projs/my_decode.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python
-
+# -*- coding: utf-8
+# FIXME: hack to force Python run 
+# export PYTHONIOENCODING=UTF-8
 import re
 import sys
 import urllib2
 from optparse import OptionParser
 from HTMLParser import HTMLParser
 
-baseurl="http://www.lingvo-online.ru/ru/Translate/en-ru/"
+#baseurl="http://www.lingvo-online.ru/ru/Translate/en-ru/"
+baseurl="http://slovari.yandex.ru/%s/перевод"
 
 def usage():
     print """
@@ -20,15 +23,25 @@ class MyTestParser(HTMLParser):
     def __init__(self):
         HTMLParser.__init__(self)
         self.recording = 0
-        self.data = []
+        self.data = u""
     def handle_data(self, data):
+        if self.recording == 1:
+            self.data += data
         pass
     def handle_starttag(self, tag, attrs):
         if tag != 'div':
             return
+        if len(attrs) == 0:
+            return
+        for item in attrs:
+            if item[0] != u"class":
+                return;
+            if item[1] != 'b-content b-content_type_lingvo':
+                return
+        debug("div finded, attrs %s" %attrs)
+        self.recording += 1
+        pass
        
-        if u'js-article-html g-card' in attrs[0]:
-            debug("div finded, attrs %s" %attrs)
     def handle_endtag(self, tag):
         pass
 
@@ -48,23 +61,23 @@ def get_description(url):
     p = MyTestParser()
     p.feed(page)
     p.close()
-
-    desc = "Stub now"
+    desc = p.data
     return desc
 
 def create_url(translateme):
     translateme = re.sub("\s+", "%20", translateme)
-    url = baseurl + translateme
+    url = baseurl % translateme
     return url
 
 def main(argc, argv):
-    debug("len of argc = %d" % argc)
+    debug("[+] len of argc = %d" % argc)
     if (argc < 2):
         usage()
         exit(1)
     url = create_url(''.join(argv[1:]))
-    debug(url)
+    debug("[+] Created url = %s" % url)
     desc = get_description(url)   
+    print type(desc)
     print desc
 
 if __name__=='__main__':
